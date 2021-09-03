4d651ba74acd075354d316db1941c2ddb78ed918
Javier <framirez76@uabc.edu.mx>
2021-09-02 23:47:10 -0700

    findIndex() method

--- 07 - Array Cardio Day 2/main.js
+++ 07 - Array Cardio Day 2/main.js
@@ -28,13 +28,22 @@ const allAdults = people.every(person => {
     (new Date).getFullYear - person.year >= 19;
 })
 
-      // Array.prototype.find()
-      // Find is like filter, but instead returns just the one you are looking for
-      // find the comment with the ID of 823423
-      const comment = comments.find(comment => {
-          return comment.id==823423;
-      })
-      console.log(comment);
-      // Array.prototype.findIndex()
-      // Find the comment with this ID
-      // delete the comment with the ID of 823423
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const comment = comments.find(comment => {
+    return comment.id == 823423;
+})
+console.log(comment);
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+
+//comments.splice(index,1)
+
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+];
