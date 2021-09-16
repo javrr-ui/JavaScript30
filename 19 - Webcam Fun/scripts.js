2de6265020330b797776232d8a41de80c5920737
Javier <framirez76@uabc.edu.mx>
2021-09-15 21:23:56 -0700

    Red effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -6,30 +6,44 @@ const snap = document.querySelector('.snap');
 
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(localMediaStream => {
-        console.log(localMediaStream);
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
 
-        // video.src = window.URL.createObjectURL(localMediaStream);
-        video.srcObject = localMediaStream;
-        video.play();
-    })
-    .catch(err => {
-        console.log(err);
-    });
+            // video.src = window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
 getVideo();
 
-function paintToCanvas(){
+function paintToCanvas() {
     const width = video.videoWidth;
     const height = video.videoHeight;
     canvas.width = width;
     canvas.height = height;
 
     return setInterval(() => {
-        ctx.drawImage(video,0,0,width, height);
-    },16)
+        ctx.drawImage(video, 0, 0, width, height);
+
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = redEffect(pixels);
+        ctx.putImageData(pixels, 0, 0);
+
+    }, 16)
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+    }
+    return pixels;
 }
 
 function takePhoto() {
@@ -42,9 +56,9 @@ function takePhoto() {
     const link = document.createElement("a");
     link.href = data;
     link.setAttribute("download", "XD")
-    link.innerHTML =  `<img src="${data}" />`;
+    link.innerHTML = `<img src="${data}" />`;
     strip.insertBefore(link, strip.firstChild);
 }
 
 
-video.addEventListener("canplay",paintToCanvas);
+video.addEventListener("canplay", paintToCanvas);
