3ec6ebd5013162ec4286ad2ca98227ac1d067d1d
Javier <framirez76@uabc.edu.mx>
2021-09-16 16:40:06 -0700

    Finished

---
+++ 20 - Speech Detection/main.js
@@ -0,0 +1,36 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = "en-US"
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+
+words.appendChild(p);
+
+
+// console.log(recognition)
+
+recognition.addEventListener("result", e=> {
+    // console.log(e.results);
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("")
+
+    p.textContent = transcript;
+    if(e.results[0].isFinal){
+        p = document.createElement("p");
+        words.appendChild(p);
+    }
+
+    if(transcript.includes("unicorn")){
+        console.log("🦄🦄🦄🦄🦄🦄🦄🦄🦄");
+    }
+
+    console.log(transcript)
+})
+
+recognition.addEventListener("end", recognition.start);
+
+recognition.start();
