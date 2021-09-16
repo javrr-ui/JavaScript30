0aedea4247ea4882564652eaa0f2b569021ed5ac
Javier <framirez76@uabc.edu.mx>
2021-09-15 21:29:33 -0700

    Rgb split

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -31,7 +31,8 @@ function paintToCanvas() {
         ctx.drawImage(video, 0, 0, width, height);
 
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
         ctx.putImageData(pixels, 0, 0);
 
     }, 16)
@@ -42,6 +43,15 @@ function redEffect(pixels) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
         pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //red
+        pixels.data[i + 100] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; //blue
     }
     return pixels;
 }
