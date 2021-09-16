01d08ae38b05497c63ebe826dd68f364599af79b
Javier <framirez76@uabc.edu.mx>
2021-09-15 20:14:05 -0700

    Paint to canvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,4 +23,11 @@ getVideo();
 
 function paintToCanvas(){
     const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video,0,0,width, height);
+    },16)
 }
