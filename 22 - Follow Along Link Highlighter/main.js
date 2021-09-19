ed9cea72d68a3ed880ba93225002bda4bb85759b
Javier <framirez76@uabc.edu.mx>
2021-09-19 15:44:20 -0700

    Add event listener when mouse enter

--- 22 - Follow Along Link Highlighter/main.js
+++ 22 - Follow Along Link Highlighter/main.js
@@ -1,1 +1,16 @@
 // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+
+document.body.appendChild(highlight);
+
+triggers.forEach(a=>{
+    a.addEventListener("mouseenter",highlightLink);
+})
+
+
+function highlightLink(){
+    console.log("Highlight!!")
+}
